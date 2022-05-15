b487b5f17b9a667407cbc1eeef050dc6fcda0d03
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-05-15 08:55:11 +0700

    Latihan 3b

---
+++ latihan_3b/latihan_3b.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	x, y := 21, 17
+	fmt.Println("x: ", x)
+	fmt.Println("y: ", y)
+	fmt.Println("======")
+	x -= 5
+	y += 3
+	fmt.Println("x: ", x)
+	fmt.Println("y: ", y)
+}
