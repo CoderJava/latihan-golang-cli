1d5e17747a3b199f2a81f0ad178853d5291b3510
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-05-17 21:30:04 +0700

    Latihan 5d

---
+++ latihan_5d/latihan_5d.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("Masukkan nilai baris: ")
+	scanner.Scan()
+	inputBaris := scanner.Text()
+	baris, err := strconv.Atoi(inputBaris)
+	if err != nil {
+		fmt.Println("Input baris tidak valid. Error:", err.Error())
+		return
+	} else if baris <= 0 {
+		fmt.Println("Inputan baris minimal bernilai 1 ya.")
+		return
+	}
+
+	printOutIncrementStyle(baris)
+	fmt.Println()
+	printOutDecrementStyle(baris)
+}
+
+func printOutIncrementStyle(jumlahBaris int) {
+	/**
+	11111
+	2222
+	333
+	44
+	5
+	*/
+
+	for x := 1; x <= jumlahBaris; x++ {
+		for y := jumlahBaris; y >= x; y-- {
+			fmt.Printf("%d", x)
+		}
+		fmt.Println()
+	}
+}
+
+func printOutDecrementStyle(jumlahBaris int) {
+	// TODO: Buat output untuk decrement style
+	/**
+	55555
+	4444
+	333
+	22
+	1
+	*/
+
+	for x := jumlahBaris; x >= 1; x-- {
+		for y := 1; y <= x; y++ {
+			fmt.Printf("%d", x)
+		}
+		fmt.Println()
+	}
+}
